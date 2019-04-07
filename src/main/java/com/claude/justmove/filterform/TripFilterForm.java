d01b91e8ae20147c4596ffec3b9a711dc4d022e4
rodrigue <affodogandji.rodrigue@gmail.com>
2019-04-07 20:59:40 +0200

    add lombok annotation to tripFilterForm

--- src/main/java/com/claude/justmove/filterform/TripFilterForm.java
+++ src/main/java/com/claude/justmove/filterform/TripFilterForm.java
@@ -1,33 +1,11 @@
 package com.claude.justmove.filterform;
 
 import com.claude.justmove.domain.Trip;
+import lombok.Data;
 
+@Data
 public class TripFilterForm {
     private Trip trip;
     private boolean sortByPriceAsc;
     private boolean sortByPriceDesc;
-
-    public void setTrip(Trip trip) {
-        this.trip = trip;
-    }
-
-    public Trip getTrip() {
-        return trip;
-    }
-
-    public boolean isSortByPriceAsc() {
-        return sortByPriceAsc;
-    }
-
-    public void setSortByPriceAsc(boolean sortByPriceAsc) {
-        this.sortByPriceAsc = sortByPriceAsc;
-    }
-
-    public boolean isSortByPriceDesc() {
-        return sortByPriceDesc;
-    }
-
-    public void setSortByPriceDesc(boolean sortByPriceDesc) {
-        this.sortByPriceDesc = sortByPriceDesc;
-    }
 }
